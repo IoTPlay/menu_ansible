c9cd9fc0f599ea5f7240dcb2a74cd36269a47f21
Jéan Roux <IoTPlay@users.noreply.github.com>
2021-01-26 08:29:44 +0200

    Update ans.zsh

--- ans-menu/ans.zsh
+++ ans-menu/ans.zsh
@@ -1,76 +1,67 @@
 #!/bin/zsh
-
 clear
+
 # ======= CHANGE ONLY THIS ==================================
 ans_home="$HOME/iotplay/ops_ansmenus/ansible"
-
 source "$ans_home/ans-menu/ans_xcfg.ini"
 cd $ans_menus
 #-------------- ans_menu.ini Exist? -------------------------
 if [ ! -f "$ans_home/ans-menu/ans_menu.ini" ]; then
     echo "ans_ini=ans_1.ini" >> "$ans_home/ans-menu/ans_menu.ini"
 fi
-#------------------------------------------------------------
 
 # ------the menu ini ----------------------------------------
 inv_limit_file="$ans_home/inventory/inventory_limit.ini" 
 source "$inv_limit_file"
 source "$ans_home/ans-menu/ans_menu.ini"  
-source "$ans_home/ans-menu/$ans_ini" # from ans_menu.ini
+source "$ans_home/ans-menu/menus/$ans_ini" # from ans_menu.ini
 
+# --- Variables----------------------------------------------
+testEcho=0 # Error Reporting
+# $1 is the first variable after the command, like 'ans 1'
+CommandDesc="a$1"       
+CommandString="c$1"
+
+if [[ $testEcho == 1 ]]; then   # Error Reporting
+    echo "Log: Variable TestEcho is = 1"
+    echo "Log: var ans_menus is = $ans_menus"
+    echo "Log: vars CommandDesc = $CommandDesc , and CommandString = $CommandString";
+    echo "Log: ans_menu_count = $ans_menu_count ";
+fi
 # ------ env variables export--------------------------------
 export ANSIBLE_CONFIG=$ans_home
 export DOCKER_CLIENT_TIMEOUT=600
 export COMPOSE_HTTP_TIMEOUT=600
 
-# ---Some Variables------------------------------------------
-#ansNo_play="ans$1_play"   # ie. ans2_play
-CommandDesc="a$1"       
-CommandString="c$1"        #  The Command string     ie. cmd3
-#echo "test.......$ansNo_play = "${!ansNo_play}""
-
 #------------------------------------------------------------
 echo ""
-echo "|==== MENU SYSTEM: $ans_menu_name  ================"
+echo "|==== MENU SYSTEM: $ans_menu_name ================"
 echo "|     with inv_limit:[$limit], menu host: [$host_used] "
-echo "|-----------------------------------------------------------------------------|"
+echo "|---------------------------------------------------------------------|"
 #-----------1st cmdline for Ansible commands ----------------------------------------
-case "$1" in
-
-"")
-    #echo "Menu:"
-    #for i in {0..40}
+case ${1} in 
+"") # if $1 is empty
     for (( i=0; i<=$ans_menu_count; i++ ))
         do
-            #eval var1="ans"$i"_opt"    #creating a var ans1_opt
             eval desc="a"$i             #Creating a var ans1_desc
             #echo "${!var1} : ${!var2}"
-            echo "$i : ${!desc}"
+            # In bash this works # echo "$i : ${!desc}"
+            # eval "value=\"\${$desc}\"" # ${${desc}}
+            echo "$i : ${(P)desc}"
         done ;
     echo "----------------------------------------------------------------------"
-    #echo "Now choose an option, ie. <bash ans.sh 1> ext dabar>"
-    echo "..cmdline 1 = <1..x> - for the action."
-    #echo "..cmdline 2 = int/ext - to use defined hosts file."
+    echo "..cmdline 1 = <1..x> - for the action." 
     echo "----------------------------------------------------------------------"
     ;;
 
-"-a")
-    echo "--ans_x.ini to choose ----------------------------------------"
+[1-50]*) # if $1 is not empty
 
-
-    echo "----------------------------------------------------------------------"
-    ;;
-
-    "$1")
-      echo "----menu option $1: Ansible Command: ${!CommandDesc}----------------------------";
-      echo "....---the command is:---"
-      echo "....... <${!CommandString}>"
-      echo "......."
-      eval "${!CommandString}"
-      echo
-
-    #   read -t 1 -p "Returning to menu..." ; echo ;
-    #   source $vartoolsdir/menu-central.sh
-    #   exit;
+    echo "----menu option $1: Ansible Command: ${(P)CommandDesc}----------------------------"; 
+    echo ".... <Command: ${(P)CommandString}>"
+    echo "...."
+    eval ${(P)CommandString}
+    echo
+    exit;
     ;;
 esac
+
