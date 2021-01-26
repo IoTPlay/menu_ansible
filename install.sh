67a4370e9c898cd37c7781a645e6be5095e7feb6
Jéan Roux <jean@iotplay.org>
2021-01-26 18:54:58 +0200

    upd.

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ varLoc2=GitHub
 varLoc3=menu_ansible
 gitRepo=https://github.com/IoTPlay/menu_ansible.git
 
-mkdir $HOME/$varloc1 $$ mkdir $HOME/$varloc1/$varloc2 && mkdir $$ mkdir $HOME/$varloc1/$varloc2/$varloc3
+mkdir $HOME/$varloc1
+mkdir $HOME/$varloc1/$varloc2  
+mkdir $HOME/$varloc1/$varloc2/$varloc3
 cd $varLoc1/$varloc2/$varloc3
 
 git clone $gitRepo
@@ -19,7 +21,8 @@ varloc1=iotplay
 varLoc1=ops_ans_menus
 gitRepo=https://github.com/IoTPlay/ops_ansmenus.git
 
-mkdir $HOME/$varloc1 $$ mkdir $HOME/$varloc1/$varloc2
+mkdir $HOME/$varloc1
+mkdir $HOME/$varloc1/$varloc2
 cd $varLoc1/$varloc2
 
 git clone $gitRepo
