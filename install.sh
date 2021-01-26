49ecac2ea6cfb7c8496d131136e5e686d4adc425
Jéan Roux <jean@iotplay.org>
2021-01-26 19:09:08 +0200

    upd.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # wget -O - https://raw.githubusercontent.com/IoTPlay/menu_ansible/master/install.sh | sh
 
 # --- clone the menu_ansible system, and ops_ansmenus and Ansible playbooks from github ------------------
+
 varloc1=iotplay/GitHub/menu_ansible
 varloc2=iotplay/ops_ans_menus
 gitRepo1=https://github.com/IoTPlay/menu_ansible.git
@@ -11,17 +12,17 @@ gitRepo2=https://github.com/IoTPlay/ops_ansmenus.git
 
 mkdir -p $HOME/$varloc1
 mkdir -p $HOME/$varloc2
-cd $varLoc1
-git clone $gitRepo1
-cd $varLoc2
-git clone $gitRepo2
+#cd $varLoc1
+#git clone $gitRepo1
+#cd $varLoc2
+#git clone $gitRepo2
 
 # --- Install ansible, docker, python, zsh ---------------------------
 # --- Ubuntu, RPi ---
-sudo apt -y update && sudo apt -y upgrade
-#sudo apt install pip3
-sudo apt -y install ansible
-sudo apt -y install python3
-sudo apt -y install zsh         # https://symeonchen.com/2018/03/11/install_zsh_on_the_raspberry_pi/
+#sudo apt -y update && sudo apt -y upgrade
+ #sudo apt install pip3
+#sudo apt -y install ansible
+#sudo apt -y install python3
+#sudo apt -y install zsh         # https://symeonchen.com/2018/03/11/install_zsh_on_the_raspberry_pi/
 # --- settings ---
-chsh -s /bin/zsh # Change default shell to zsh
+#chsh -s /bin/zsh # Change default shell to zsh
