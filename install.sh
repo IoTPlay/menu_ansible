00fda6df52d91693f6f513222e5176a208bac697
Jéan Roux <jean@iotplay.org>
2021-01-30 21:53:44 +0200

    menu options

--- install.sh
+++ install.sh
@@ -15,25 +15,44 @@ if [ $testEcho == 1 ]; then   # Error Reporting
   echo "...."
 fi 
 
-function cloneinDir () {
-  #echo "....errorlog: var1 = "$1", var2 = "$2" "
-  mkdir -p "${HOME}/$1"
-  git clone --depth 1 $2 "${HOME}/$1" 
-}
+case ${1} in 
 
-cloneinDir ${varloc1} ${gitRepo1}
-cloneinDir ${varloc2} ${gitRepo2}
+  "")
+    function cloneinDir () {
+      #echo "....errorlog: var1 = "$1", var2 = "$2" "
+      mkdir -p "${HOME}/$1"
+      git clone --depth 1 $2 "${HOME}/$1" 
+    }
+    cloneinDir ${varloc1} ${gitRepo1}
+    cloneinDir ${varloc2} ${gitRepo2}
 
-# --- Install ansible, docker, python, zsh ---------------------------
-# --- Ubuntu, RPi ---
-sudo apt -y update && sudo apt -y upgrade
- #sudo apt install pip3
-sudo apt -y install ansible
-sudo apt -y install python3
-sudo apt -y install zsh         # https://symeonchen.com/2018/03/11/install_zsh_on_the_raspberry_pi/
-# --- settings ---
-sudo chsh -s /bin/zsh # Change default shell to zsh
+    # --- Install ansible, docker, python, zsh ---------------------------
+    # --- Ubuntu, RPi ---
+    sudo apt -y update && sudo apt -y upgrade
+    #sudo apt install pip3
+    sudo apt -y install ansible
+    sudo apt -y install python3
+    sudo apt -y install zsh         # https://symeonchen.com/2018/03/11/install_zsh_on_the_raspberry_pi/
+    # --- settings ---
+    sudo chsh -s /bin/zsh # Change default shell to zsh
+    echo "alias ans="zsh $HOME/iotplay/GitHub/menu_ansible/ans.zsh" " >> ${HOME}/.zshenv
+    echo "alias ans="zsh $HOME/iotplay/GitHub/menu_ansible/ans.zsh" " >> ${HOME}/.bashrc
+    ;;
+  
+  "--help")
+    echo "Run command with following variables for: "
+    echo "  --help   - for help"
+    echo "  --system - clones only the menu system"
+    echo "  --menu   - clones only the menus and playbooks"
+    echo "  without any variables, it will mkdir, clone github menus and system, upd. SW, and install ansible"
+  ;;
 
-echo "alias ans="zsh $HOME/iotplay/GitHub/menu_ansible/ans.zsh" " >> ${HOME}/.zshenv
+  "--system")
+      cloneinDir ${varloc1} ${gitRepo1}
+  ;;
+  
+  "--menu")
+    cloneinDir ${varloc2} ${gitRepo2}
+  ;;
 
-echo "alias ans="zsh $HOME/iotplay/GitHub/menu_ansible/ans.zsh" " >> ${HOME}/.bashrc
+esac
